5d3e588d4340f42a415b2dad33be2b5f8b321c65
idevnva <127990298+idevnva@users.noreply.github.com>
2023-04-08 22:51:22 +0300

    018

--- iWallet/View/Transaction/TransactionView.swift
+++ iWallet/View/Transaction/TransactionView.swift
@@ -9,13 +9,6 @@ struct TransactionView: View {
     @ObservedResults(TransactionItem.self) var transactions
     @ObservedResults(Category.self) var categories
     
-    private let dateFormatter: DateFormatter = {
-        let formatter = DateFormatter()
-        formatter.dateStyle = .medium
-        formatter.timeStyle = .none
-        return formatter
-    }()
-    
     var body: some View {
         NavigationStack {
             List {
@@ -40,20 +33,28 @@ struct TransactionView: View {
                             .foregroundColor(.gray)
                             .font(.system(size: 12))
                         Spacer(minLength: 20)
-                }
-                .frame(maxWidth: .infinity, maxHeight: 300)
+                    }
+                    .frame(maxWidth: .infinity, maxHeight: 300)
                     
                 } else {
                     
-                    ForEach(transactions.reversed(), id: \.self) { transaction in
-                        let matchingCategories = categories.filter { $0.id == transaction.categoryId }
-                        if let category = matchingCategories.first {
-                            transactionRow(transaction: transaction, category: category)
+                    let groupedTransactions = transactionsByDate(Array(transactions))
+                    
+                    ForEach(groupedTransactions.keys.sorted(by: { $0 > $1 }), id: \.self) { date in
+                        Section(header: Text(date, style: .date).bold()) {
+                            let sortedTransactions = sortTransactionsByDate(transactions: groupedTransactions[date]!)
+                            
+                            ForEach(sortedTransactions, id: \.self) { transaction in
+                                if let category = filterCategories(categories: Array(categories), transaction: transaction) {
+                                    transactionRow(transaction: transaction, category: category)
+                                }
+                            }
+                            .onDelete(perform: deleteTransaction)
                         }
                     }
-                    .onDelete(perform: deleteTransaction)
                 }
             }
+            .listStyle(GroupedListStyle())
             .navigationBarTitle("Транзакции", displayMode: .inline)
             .toolbar {
                 ToolbarItem(placement: .navigationBarLeading) {
@@ -69,52 +70,103 @@ struct TransactionView: View {
         }
     }
     
+    // Метод для оптимизации отображения списка
     private func transactionRow(transaction: TransactionItem, category: Category) -> some View {
-         VStack(alignment: .leading, spacing: 0) {
-             HStack {
-                 HStack {
-                     Divider()
-                         .foregroundColor(Color(category.color))
-                         .frame(width: 5, height: 150)
-                         .background(Color(category.color))
-                 } .padding(.trailing, 3)
-                 
-                 VStack(alignment: .leading) {
-                     HStack {
-                         Text(transaction.amount.formattedWithSeparatorAndCurrency())
-                             .font(.title3).bold()
-                         Spacer()
-                         HStack {
-                             Text(category.name)
-                                 .foregroundColor(Color("colorBalanceText")).textCase(.uppercase)
-                                 .font(.caption)
-                                 .multilineTextAlignment(.trailing)
-                                 .dynamicTypeSize(.small)
-                                 .padding(0)
-                             Image(systemName: category.icon)
-                                 .font(.caption).dynamicTypeSize(.small)
-                                 .foregroundColor(.black)
-                                 .frame(width: 20, height: 20)
-                                 .background(Color(category.color))
-                                 .cornerRadius(5)
-                                 .padding(0)
-                         } .padding(0)
-                     }
-                     Text(transaction.note)
-                         .foregroundColor(Color(.gray)).textCase(.uppercase)
-                         .font(.subheadline).dynamicTypeSize(.small)
-                 }    .padding(.leading, 10)
-             }
-         }
-         .padding(.vertical, 5)
-         .frame(height: 50)
-     }
-    
-    private func deleteTransaction(at offsets: IndexSet) {
-        offsets.forEach { index in
-            let transaction = transactions.reversed()[index]
-            viewModel.deleteTransaction(withId: transaction.id)
+        VStack(alignment: .leading, spacing: 0) {
+            HStack {
+                HStack {
+                    Divider()
+                        .foregroundColor(Color(category.color))
+                        .frame(width: 5, height: 72)
+                        .background(Color(category.color))
+                } .padding(.trailing, 3)
+                
+                VStack(alignment: .leading) {
+                    HStack {
+                        if transaction.type == CategoryType.expense {
+                            Text("-\(transaction.amount.formattedWithSeparatorAndCurrency())")
+                                .font(.title3).bold()
+                        } else {
+                            Text(transaction.amount.formattedWithSeparatorAndCurrency())
+                                .font(.title3).bold()
+                        }
+                        Spacer()
+                        HStack {
+                            Text(category.name)
+                                .foregroundColor(Color("colorBalanceText")).textCase(.uppercase)
+                                .font(.caption)
+                                .multilineTextAlignment(.trailing)
+                                .dynamicTypeSize(.small)
+                                .padding(0)
+                            Image(systemName: category.icon)
+                                .font(.caption).dynamicTypeSize(.small)
+                                .foregroundColor(.black)
+                                .frame(width: 20, height: 20)
+                                .background(Color(category.color))
+                                .cornerRadius(5)
+                                .padding(0)
+                        } .padding(0)
+                    }
+                    HStack {
+                        Text(transaction.note)
+                            .foregroundColor(Color(.gray)).textCase(.uppercase)
+                            .font(.subheadline).dynamicTypeSize(.small)
+                        Spacer()
+                        Text(category.type.rawValue)
+                            .foregroundColor(Color(.gray)).textCase(.uppercase)
+                            .font(.subheadline).dynamicTypeSize(.small)
+                    }
+                }    .padding(.leading, 10)
+            }
         }
+        .padding(.vertical, 5)
+        .frame(height: 50)
+    }
+    
+    // Метод удаления транзакций
+    private func deleteTransaction(at offsets: IndexSet) {
+        withAnimation {
+            offsets.forEach { index in
+                let transaction = transactions.reversed()[index]
+                viewModel.deleteTransaction(withId: transaction.id)
+            }
+        }
+    }
+    
+    // Метод для группировки транзакций по дате
+    private func transactionsByDate(_ transactions: [TransactionItem]) -> [Date: [TransactionItem]] {
+        var groupedTransactions: [Date: [TransactionItem]] = [:]
+        
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateStyle = .medium
+        dateFormatter.timeStyle = .none
+        
+        for transaction in transactions {
+            let dateString = dateFormatter.string(from: transaction.date)
+            if let date = dateFormatter.date(from: dateString) {
+                if groupedTransactions[date] == nil {
+                    groupedTransactions[date] = []
+                }
+                groupedTransactions[date]?.append(transaction)
+            }
+        }
+        
+        return groupedTransactions
+    }
+    
+    // Метод фильтрации категорий
+   private func filterCategories(categories: [Category], transaction: TransactionItem) -> Category? {
+        for category in categories {
+            if category.id == transaction.categoryId {
+                return category
+            }
+        }
+        return nil
+    }
+    
+    // метод сортировки транзакций по дате
+    private func sortTransactionsByDate(transactions: [TransactionItem]) -> [TransactionItem] {
+        return transactions.sorted(by: { $0.date > $1.date })
     }
 }
 
